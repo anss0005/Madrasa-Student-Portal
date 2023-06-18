e90381fc915555e02cfe466bf02faeeab2c1f6d7
shehz <anssraza41@gmail.com>
2023-06-18 15:07:26 +0500

    commit6

---
+++ app/src/main/java/DBHelper.java
@@ -0,0 +1,42 @@
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "StudentDB";
+    private static final String TABLE_NAME = "students";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_AGE = "age";
+    private static final String COLUMN_CLASS = "class";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_AGE + " INTEGER,"
+                + COLUMN_CLASS + " TEXT"
+                + ")";
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/madrasastudent/EntryForm.java
+++ app/src/main/java/com/example/madrasastudent/EntryForm.java
@@ -3,12 +3,36 @@ package com.example.madrasastudent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EntryForm extends AppCompatActivity {
 
+    EditText editTextId, editTextName, editTextAge, editTextClass;
+     Button buttonSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_form);
+
+
+        // Find views by their IDs
+        editTextId = findViewById(R.id.editTextId);
+        editTextName = findViewById(R.id.editTextName);
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextClass = findViewById(R.id.editTextClass);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
     }
 }
