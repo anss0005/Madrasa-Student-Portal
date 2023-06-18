a28bae656d78a69631438491f869c957918827c2
shehz <anssraza41@gmail.com>
2023-06-18 14:06:31 +0500

    commit4

---
+++ app/src/main/java/com/example/madrasastudent/EntryForm.java
@@ -0,0 +1,14 @@
+package com.example.madrasastudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntryForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_form);
+    }
+}
--- app/src/main/java/com/example/madrasastudent/MainActivity.java
+++ app/src/main/java/com/example/madrasastudent/MainActivity.java
@@ -2,17 +2,44 @@ package com.example.madrasastudent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button enterBtn,SearchBtn,GitBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Binding ui elements
+        enterBtn=findViewById(R.id.entrBtn);
+        SearchBtn=findViewById(R.id.searchBtn);
+        GitBtn=findViewById(R.id.gitBtn);
+
+
+
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,EntryForm.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
+
+
 
 
 
